1b88f6cfd4f520bd1bf2ba53a317fb9a404b6840
DomnulCiot <cristiciotei@gmail.com>
2018-05-14 23:00:49 +0300

    Menu_1

---
+++ charcontroller/Assets/Pause_Menu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour {
+
+    public static bool IsPaused = false;
+    public GameObject Pause_UI;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused == false)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+	}
+
+    void Pause ()
+    {
+        Pause_UI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+   public void Resume ()
+    {
+        Pause_UI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false; 
+    }
+
+    public void Load_MainMenu ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+
+}
