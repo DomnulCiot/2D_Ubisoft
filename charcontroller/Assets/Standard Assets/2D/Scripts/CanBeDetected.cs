27cb33120ae32353bfc620b79610e0975b9e853a
Alex <alextove02@gmail.com>
2018-05-19 18:56:07 +0300

    Un pic de enemyAI

---
+++ charcontroller/Assets/Standard Assets/2D/Scripts/CanBeDetected.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanBeDetected : MonoBehaviour
+{
+
+    public bool PlayerHidden = false;
+    public bool gotoplayer = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && !PlayerHidden)
+        {
+            Debug.Log("Detected Player");
+            gotoplayer = true;
+        }
+    }
+}
--- charcontroller/Assets/Standard Assets/2D/Scripts/EnemyMovementControl.cs
+++ charcontroller/Assets/Standard Assets/2D/Scripts/EnemyMovementControl.cs
@@ -6,15 +6,25 @@ public class EnemyMovementControl : MonoBehaviour {
 
     public Vector3 pos1 = new Vector3(-4, 0, 0);
     public Vector3 pos2 = new Vector3(4, 0, 0);
-    public float speed = 1.0f;
+    public float speed = 10.0f;
+    private CanBeDetected player;
+    public Transform target;
 
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<CanBeDetected>();
+    }
     void Update()
     {
-        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
-        
-    }
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        
+        if (player.gotoplayer == true)
+        {
+            if (Vector3.Distance(transform.position, target.position) > 1f)
+            {
+                if(transform.position.x > target.position.x)
+                    transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+                else
+                    transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+        }
     }
 }
