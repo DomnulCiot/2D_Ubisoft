df79cf98f485fc9c00f3fc4cc26faaee3d427f63
Alex <alextove02@gmail.com>
2018-05-20 20:58:51 +0300

    Ceva prototip de Elder

---
+++ charcontroller/Assets/DisplayText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText : MonoBehaviour {
+
+    private Queue<string> sentences;
+    public Canvas canvas;
+    void Start()
+    {
+        sentences = new Queue<string>();
+        canvas.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            canvas.enabled = true;
+            Debug.Log("Sunt un Elder GAY");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            canvas.enabled = false;
+            Debug.Log("Sunt un Elder GAY");
+        }
+    }
+}
