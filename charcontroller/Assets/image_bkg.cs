bc7ef37c56673c785307a9a66f5c9a66d0f031a2
DomnulCiot <cristiciotei@gmail.com>
2018-05-26 02:54:02 +0300

    monis 2

---
+++ charcontroller/Assets/image_bkg.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class image_bkg : MonoBehaviour {
+
+    public Sprite[] Img;
+    public int count = 0;
+    public Image img;
+	
+	
+
+   public void NextImg ()
+    {
+        count++;
+        if (count<2)
+        img.sprite = Img[count];
+        else
+        if (count == 2)
+        {
+            Debug.Log("monis");
+            SceneManager.LoadScene("yes");
+        }
+            
+    }
+}
